7c0c6f8737d2afa83dd6630530d43b1492bc2a2d
Balamathias <balamathias05@gmail.com>
2024-05-08 15:59:13 +0100

    Implement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """function that queries the Reddit API and returns the number of
     subscribers for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'User-Agent: windows:com.aora.matiecodes:v1.2.3 (by /u/matiecodes)'}
+    headers = {'User-Agent': 'windows:com.aora.matiecodes:v1.2.3 (by /u/matiecodes)'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
