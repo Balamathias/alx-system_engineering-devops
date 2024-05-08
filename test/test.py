6aceec440b531557a50c5c7f2cdce18bf1044d53
Balamathias <balamathias05@gmail.com>
2024-05-08 15:53:57 +0100

    Implement

--- test/test.py
+++ test/test.py
@@ -1,18 +1,41 @@
-def say_hello(name: str) -> str:
-    return f"Hello, {name}!"
+import requests
 
-# Path: test/test.py
-def say_goodbye(name: str) -> str:
-    return f"Goodbye, {name}!"
+url = 'http://localhost:5000/api/users'
 
-# Path: test/test.py
-def say_hello_and_goodbye(name: str) -> str:
-    return f"{say_hello(name)} {say_goodbye(name)}"
+user_info = {
+    'username': 'serafina',
+    'password': 'serafina',
+    'email': 'serafina@gmail.com'
+}
 
-# Path: test/test.py
-def say_hello_and_goodbye_twice(name: str) -> str:
-    return f"{say_hello_and_goodbye(name)} {say_hello_and_goodbye(name)}"
+headers = {
+    'Content-Type': 'Application/json',
+}
 
-# Path: test/test.py
-def say_hello_and_goodbye_thrice(name: str) -> str:
-    return f"{say_hello_and_goodbye_twice(name)} {say_hello_and_goodbye(name)}"
+response = requests.post(url, json=user_info, headers=headers)
+
+print(response.json())
+
+
+# def say_hello(name: str) -> str:
+#     return f"Hello, {name}!"
+
+# # Path: test/test.py
+# def say_goodbye(name: str) -> str:
+#     return f"Goodbye, {name}!"
+
+# # Path: test/test.py
+# def say_hello_and_goodbye(name: str) -> str:
+#     return f"{say_hello(name)} {say_goodbye(name)}"
+
+# # Path: test/test.py
+# def say_hello_and_goodbye_twice(name: str) -> str:
+#     return f"{say_hello_and_goodbye(name)} {say_hello_and_goodbye(name)}"
+
+# # Path: test/test.py
+# def say_hello_and_goodbye_thrice(name: str) -> str:
+#     return f"{say_hello_and_goodbye_twice(name)} {say_hello_and_goodbye(name)}"
+
+# square = lambda x : x ** 2
+
+# print(square(5))
